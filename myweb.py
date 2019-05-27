4c606839fd2a9a04da8038184b71cb0f74e42898
ljx@botsally.com <ljx@botsally.com>
2019-05-27 18:53:40 +0800

    demo

--- myweb.py
+++ myweb.py
@@ -1,1 +1,11 @@
-import flask
+from flask import Flask
+from flask import render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'hello'
+
+if __name__ == '__main__':
+    app.run()
